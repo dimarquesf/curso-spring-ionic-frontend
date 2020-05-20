ee123cd1ac93214b351fd0199e93f478aaea56c8
Diego Marques <fariia_di@hotmail.com>
2020-05-19 23:15:29 -0300

    Tela de confirmacao de pedido

---
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartItem } from '../../models/cart-item';
+import { EnderecoDTO } from '../../models/endereco.dto';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { CartService } from '../../services/domain/cart.service';
+
+@IonicPage()
+@Component({
+  selector: 'page-order-confirmation',
+  templateUrl: 'order-confirmation.html',
+})
+export class OrderConfirmationPage {
+
+  pedido: PedidoDTO;
+  cartItems: CartItem[];
+  cliente: ClienteDTO;
+  endereco: EnderecoDTO;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public clienteService: ClienteService,
+    public cartService: CartService) {
+
+    this.pedido = this.navParams.get('pedido');
+  }
+
+  ionViewDidLoad() {
+    this.cartItems = this.cartService.getCart().items; //pegar carrinho e joga itens dela na variavel
+
+    this.clienteService.findByID(this.pedido.cliente.id) //faz a busca por Id
+      .subscribe(response => {
+        this.cliente = response as ClienteDTO; //casting pega todos dados de cliente e joga pra variavel
+        this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, response['enderecos']);
+      },
+      error => {
+        this.navCtrl.setRoot('HomePage');
+      });
+  }
+
+  private findEndereco(id: string, list: EnderecoDTO[]) : EnderecoDTO {
+    let position = list.findIndex(x => x.id == id);
+    return list[position];
+  }
+
+  total() : number {
+    return this.cartService.total();
+  } 
+}
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -17,6 +17,11 @@ export class ClienteService {
         return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`); // passa cabeçalho para requisição
     }
 
+    findByID(id: string)  {
+
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`); 
+    }
+
     getImageFromBucket(id : string) : Observable<any> { // buscar imagem no bucket
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg` //montando a URL
         return this.http.get(url, {responseType : 'blob'}); // requisição blob que é imagem
