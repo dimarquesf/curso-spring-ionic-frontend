93da7db7e82d6061502be4382252e782620b23d1
Diego Marques <fariia_di@hotmail.com>
2020-05-23 23:48:40 -0300

    Fazendo upload da foto

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -4,13 +4,16 @@ import { Observable } from "rxjs/Rx";
 import { Injectable } from "@angular/core";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
+import { ImageUtilService } from "../image-util-service";
 
 @Injectable()
 export class ClienteService {
 
-    constructor ( public http : HttpClient, public storage : StorageService) {
-    
-    }
+    constructor ( 
+        public http : HttpClient,
+         public storage : StorageService,
+         public imageUtilService: ImageUtilService) {
+        }
 
     findByEmail(email: string)  {
 
@@ -37,4 +40,19 @@ export class ClienteService {
             }
         ); 
     }
+
+    uploadPicture(picture) {
+        let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+        let formData : FormData = new FormData();
+        formData.set('file', pictureBlob, 'file.png');
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes/picture`, 
+            formData,
+            { 
+                observe: 'response', 
+                responseType: 'text'
+            }
+        ); 
+    }
+
 }
